b36c9b8579dffecbeea16b8173f9d247d6ed9c4e
gnomist <nukabar88@yahoo.com>
2017-11-29 21:48:58 -0500

    edit

--- sorter_thread.c
+++ sorter_thread.c
@@ -446,7 +446,8 @@ void* sortDir (void* ptrIn)
 		while ((ent = readdir(dir)) != NULL) 
 		{
 			// Create a path for each directory entry
-			char *path = (char *)malloc(256 * sizeof(char));
+			char path[256];
+			//char *path = (char *)malloc(256 * sizeof(char));
 			strcpy(path, tempDir->targetDir);
 			strcat(path, "/");
 			strcat(path, ent->d_name);
